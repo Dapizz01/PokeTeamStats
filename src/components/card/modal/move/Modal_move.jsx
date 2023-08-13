b0227976e90a28fc642dbbbd74bcc200ae456e1e
Dapizz01 <m1.dapozzo@gmail.com>
2023-08-13 19:47:30 +0200

    Added fetch moves in the modal

--- src/components/card/modal/move/Modal_move.jsx
+++ src/components/card/modal/move/Modal_move.jsx
@@ -1,23 +1,40 @@
-import React, { useRef } from 'react'
+import * as Pokedex from 'pokeapi-js-wrapper'
+import React, { useRef, useState } from 'react'
 
 function Modal_move({ moves, setSelected, setSelectedMoves }) {
     const selectRef = useRef(null)
-    const [move, setMove] = useState()
+    const [move, setMove] = useState(null)
+    const pokedex = new Pokedex.Pokedex()
 
-    useEffect(() => {}, [move])
+    async function fetch_move(moveName) {
+        const result = await pokedex.getMoveByName(moveName)
+        /* let en_entry = result.effect_entries.filter(
+            (entry) => entry.language.name == 'en'
+        )*/
+        setMove(result)
+    }
 
     return (
         <div>
             <select
                 className="select select-bordered"
                 ref={selectRef}
-                onChange={() => setMove(selectRef.current.value)}
+                onChange={() => fetch_move(selectRef.current.value)}
             >
                 {['', ...moves].map((moveName) => {
                     return <option key={moveName}>{moveName}</option>
                 })}
             </select>
-            <div>Move type: {move}</div>
+            {move !== null && (
+                <div>
+                    <div>Type: {move.type.name}</div>
+                    <div>Power: {move.power}</div>
+                    <div>PPs: {move.pp}</div>
+                    <div>
+                        Description: {move.effect_entries[0].short_effect}
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
